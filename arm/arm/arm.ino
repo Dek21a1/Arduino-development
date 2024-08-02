c0bf29c113c0ba6ee14277bdc9e656a3b46d680c
Deklan L <124547401+Dek21a1@users.noreply.github.com>
2024-08-02 14:16:57 +1000

    added sensor libraries

--- arm/arm/arm.ino
+++ arm/arm/arm.ino
@@ -1,11 +1,47 @@
 #include <Servo.h>
+`#include <wire.h>
+#include "Adafruit_TCS34725.h"
+
+byte gammatable[256];
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_50MS, TCS34725_GAIN_4X);
 
 Servo Servo1;
+
 void setup() {
-Servo1.attach(9);
-Servo1.write(90);
+  Serial.begin(9600);
+  Servo1.attach(9);
+  Servo1.write(90);
+
+    if (tcs.begin()) {
+    //Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1); // halt!
+  }
+
+  // Convert RGB colors to what humans see
+  for (int i=0; i<256; i++) {
+    float x = i;
+    x /= 255;
+    x = pow(x, 2.5);
+    x *= 255;
+
+    gammatable[i] = 255 - x;
+
+  }
 }
 
 void loop() {
-  
+    float red, green, blue;
+
+  delay(60);  // takes 50ms to read
+
+  tcs.getRGB(&red, &green, &blue);
+
+  Serial.print("R:\t"); Serial.print(int(red)); 
+  Serial.print("\tG:\t"); Serial.print(int(green)); 
+  Serial.print("\tB:\t"); Serial.print(int(blue));
+
+  Serial.print("\n");
+
 }
